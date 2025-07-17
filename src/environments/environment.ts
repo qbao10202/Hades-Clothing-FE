e3ecc0d6556712de414808c6810e970d7076daaf
qbao10202 <quocbao01652@gmail.com>
2025-07-18 02:15:53 +0700

    up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: '/api',
+  // apiUrl: '/api',
+  apiUrl: 'https://hades-clothing-be-production.up.railway.app/api',
   stripePublicKey: 'pk_test_your-stripe-public-key',
   paypalClientId: 'your-paypal-client-id',
   appName: 'Sales Application',
